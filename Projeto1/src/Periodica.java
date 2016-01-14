54c9b8a55724df63cfd811a8a1db691210f06a45
guggas <guggas@gmail.com>
2016-01-14 16:12:26 +0000

    Periodica - adicionado

--- Projeto1/src/Periodica.java
+++ Projeto1/src/Periodica.java
@@ -1,18 +1,17 @@
-
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Periodica extends Publicacao {
 
-	private String periodicidade;
+	private String periodicidade; //Hipóteses válidas=Diária, Semanal, Quinzenal, Mensal, Trimestral, Semestral ou Anual
 
-	public Periodica(String titulo, Date dataPub, String descricao, String id, int maxDiasRequisicao, String periodicidade) {
-		super(titulo, dataPub, descricao, id, maxDiasRequisicao);
-		throw new UnsupportedOperationException("The method is not implemented yet.");
-		
-		
+	public Periodica(String titulo, Date dataPub, String descricao, String id, ArrayList<Area> areas, String periodicidade) {
+		super(titulo, dataPub, descricao, id, areas);
+		this.periodicidade=periodicidade;
 	}
 	
-	
+	public String getPeriodicidade() {
+		return periodicidade;
+	}
 
 }
